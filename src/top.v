5aec23b3db73fa2d451eec610b6a637d99cf0434
Bernardo Pandolfi Costa <bernardopandolficosta@gmail.com>
2023-11-14 18:24:20 -0300

    MUITA COISA

--- src/top.v
+++ src/top.v
@@ -19,28 +19,33 @@ output   serial_tx_o;
 output [7:0]   serial_read_o;
 
 
-(*ramstyle = "M9K"*) reg [15:0] data_memory [255:0];
-()
+reg [7:0] buffer;
 
+(*ramstyle = "M9K"*) reg [7:0] data_memory [4:0];
 
 
 initial begin
-    $readmemh("path\\arquivo.txt",data_memory);
+	 $readmemh("D:\\coding\\hdl\\PIC\\docs\\arquivo.txt", data_memory);
+    $display("First line of data_memory: %h", data_memory[1]);
 end
 
+always begin
+    buffer=data_memory[1];
+end
 
 uart U00
 (
    .CLK_UART_i(CLK_UART_i),
    .serial_rx_i(serial_rx_i), 
    .start_tx_i(start_tx_i),
-   .serial_write_i(serial_write_i),
+   .serial_write_i(buffer),
    .busy_rx_o(busy_rx_o), 
    .busy_tx_o(busy_tx_o),
    .valid_rx_o(valid_rx_o),                 
    .serial_tx_o(serial_tx_o),                
    .serial_read_o(serial_read_o)
 );
+
 
 
     
