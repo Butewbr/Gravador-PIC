9a585a58cb1af399fab762b34d659fdf403e36a2
Bernardo Pandolfi Costa <bernardopandolficosta@gmail.com>
2023-11-17 13:02:18 -0300

    coisas

---
+++ src/memram.v
@@ -0,0 +1,50 @@
+// Quartus II Verilog Template
+// Single port RAM with single read/write address and initial contents 
+// specified with an initial block
+
+module single_port_ram_with_init
+#(parameter DATA_WIDTH=8, parameter ADDR_WIDTH=6)
+(
+	input [(DATA_WIDTH-1):0] data,
+	input [(ADDR_WIDTH-1):0] addr,
+	input we, clk,
+	output [(DATA_WIDTH-1):0] q
+);
+
+	// Declare the RAM variable
+	reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
+
+	// Variable to hold the registered read address
+	reg [ADDR_WIDTH-1:0] addr_reg;
+
+	integer counter, i, flag;
+
+(*ramstyle = "M9K"*) reg [9:0] data_memory [999:0];
+	
+	// Specify the initial contents.  You can also use the $readmemb
+	// system task to initialize the RAM variable from a text file.
+	// See the $readmemb template page for details.
+	initial 
+	begin : INIT
+		integer i;
+		$readmemh("D:\\coding\\hdl\\PIC\\docs\\arquivo.hex", data_memory);
+		for(i = 0; i < 2**ADDR_WIDTH; i = i + 1)
+				ram[i] = data_memory[0];
+			
+	end 
+
+	always @ (posedge clk)
+	begin
+		// Write
+		if (we)
+			ram[addr] <= data;
+
+		addr_reg <= addr;
+	end
+
+	// Continuous assignment implies read returns NEW data.
+	// This is the natural behavior of the TriMatrix memory
+	// blocks in Single Port mode.  
+	assign q = ram[addr_reg];
+
+endmodule
--- src/top.v
+++ src/top.v
@@ -1,5 +1,4 @@
 module top (
-    
    CLK_UART_i,
    serial_rx_i, 
    start_tx_i,
@@ -10,34 +9,47 @@ module top (
    serial_tx_o,                
    serial_read_o              
 );
- input  CLK_UART_i;                 
- input  serial_rx_i, start_tx_i;    
-input [7:0]   serial_write_i;             
- output  busy_rx_o, busy_tx_o;       
-output   valid_rx_o;                 
-output   serial_tx_o;                
-output [7:0]   serial_read_o;
 
+input CLK_UART_i;                 
+input serial_rx_i, start_tx_i;
+input [7:0] serial_write_i;             
+
+output busy_rx_o, busy_tx_o;       
+output valid_rx_o;                 
+output serial_tx_o;                
+output [7:0] serial_read_o;
 
 reg [7:0] buffer;
 
-(*ramstyle = "M9K"*) reg [7:0] data_memory [4:0];
+//integer counter, i, flag;
 
-
+//(*ramstyle = "M9K"*) reg [9:0] data_memory [999:0];
+/*
 initial begin
-	 $readmemh("D:\\coding\\hdl\\PIC\\docs\\arquivo.txt", data_memory);
-    $display("First line of data_memory: %h", data_memory[1]);
+	$readmemh("D:\\coding\\hdl\\PIC\\docs\\arquivo.hex", data_memory);
+   for (i=0; i<=5; i=i+1)
+      $display("Line %d of data_memory: %h", i, data_memory[i]);
+   
+   flag = 1;
 end
 
-always begin
-    buffer=data_memory[1];
+always @(start_tx_i) begin
+   for (counter=0; counter<=999; counter=counter+1)
+      buffer = data_memory[counter];
+      if(buffer == 10'h00000001FF) begin
+         counter = 999;
+         flag = 0;
+      end
+
 end
+*/
+
 
 uart U00
 (
    .CLK_UART_i(CLK_UART_i),
    .serial_rx_i(serial_rx_i), 
-   .start_tx_i(start_tx_i),
+   .start_tx_i(~start_tx_i && flag),
    .serial_write_i(buffer),
    .busy_rx_o(busy_rx_o), 
    .busy_tx_o(busy_tx_o),
@@ -46,6 +58,15 @@ uart U00
    .serial_read_o(serial_read_o)
 );
 
+single_port_ram_with_init U01
+
+(
+	.data(),
+	.addr(),
+	.we(), 
+	.clk(CLK_UART_i),
+	.q()
+);
 
 
     
